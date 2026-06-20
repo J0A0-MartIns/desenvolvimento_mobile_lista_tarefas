42986ecc4c7e7418289fd60c94573be6e523eb7f
João Marcos Martins <135861665+J0A0-MartIns@users.noreply.github.com>
2026-06-20 08:13:52 -0300

    Update api_constants.dart

--- lib/api_constants.dart
+++ lib/api_constants.dart
@@ -3,11 +3,11 @@ import 'package:flutter/foundation.dart';
 class ApiConstants {
   static String get baseUrl {
     if (kIsWeb) {
-      return 'http://127.0.0.1:3000';
+      return 'prog-mobile-api-pvzly8-035b36-132-145-196-104.sslip.io';
     } else if (defaultTargetPlatform == TargetPlatform.android) {
-      return 'http://10.0.2.2:3000';
+      return 'prog-mobile-api-pvzly8-035b36-132-145-196-104.sslip.io';
     } else {
-      return 'http://127.0.0.1:3000';
+      return 'prog-mobile-api-pvzly8-035b36-132-145-196-104.sslip.io';
     }
   }
 }
