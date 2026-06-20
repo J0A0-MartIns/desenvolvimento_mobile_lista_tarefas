413e2165ee93330181fd9299313e602ec097538b
MatheusFe23 <155886669+MatheusFe23@users.noreply.github.com>
2026-06-20 08:34:24 -0300

    Update baseUrl to use HTTP protocol

--- lib/api_constants.dart
+++ lib/api_constants.dart
@@ -3,11 +3,11 @@ import 'package:flutter/foundation.dart';
 class ApiConstants {
   static String get baseUrl {
     if (kIsWeb) {
-      return 'prog-mobile-api-pvzly8-035b36-132-145-196-104.sslip.io';
+      return 'http://prog-mobile-api-pvzly8-035b36-132-145-196-104.sslip.io';
     } else if (defaultTargetPlatform == TargetPlatform.android) {
-      return 'prog-mobile-api-pvzly8-035b36-132-145-196-104.sslip.io';
+      return 'http://prog-mobile-api-pvzly8-035b36-132-145-196-104.sslip.io';
     } else {
-      return 'prog-mobile-api-pvzly8-035b36-132-145-196-104.sslip.io';
+      return 'http://prog-mobile-api-pvzly8-035b36-132-145-196-104.sslip.io';
     }
   }
 }
